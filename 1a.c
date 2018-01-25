181d96b9cd276db84e6745c781186c8421a65ef6
sudharsan26797 <30828661+sudharsan26797@users.noreply.github.com>
2018-01-25 16:36:37 +0530

    Create 1a.c

---
+++ 1a.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+{
+int son;
+print("enter a number: \n");
+scanf("%d", &son):
+if (son > 0)
+   printf("%d is a positive number\n", son);
+else if (son < 0)
+   printf("%d" is a negative number\n", son);
+else
+   printf("0 is neither positive nor negative");
+   }
