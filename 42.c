4f796a4d2b70bab9d7058acb8766063f4f1797da
sudharsan26797 <30828661+sudharsan26797@users.noreply.github.com>
2018-03-08 16:12:59 +0530

    Create 42.c

---
+++ 42.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<string.h>
+int main(void)
+{
+char str1[10],str2[10];
+int n1,n2;
+scanf("%s",str1);
+scanf("%s",str2);
+n1=strlen(str1);
+n2=strlen(ste2);
+if(n1>n2)
+{
+printf("\n%s",str1);
+}
+else if (n2>n1)
+{
+printf("\n%s",str2);
+}
+return 0;
+}
