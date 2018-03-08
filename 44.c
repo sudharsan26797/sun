91cc68e9f1ecf1578f045c99be96b12817082dbd
sudharsan26797 <30828661+sudharsan26797@users.noreply.github.com>
2018-03-08 16:52:59 +0530

    Create 44.c

---
+++ 44.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+int n,low,high;
+scanf("%d",low);
+scanf("%d",high);
+scanf("%d",num);
+((num=low)&&(num=hig))
+{
+printf("number include");
+}
+else
+{
+printf("not include");
+}
+}
